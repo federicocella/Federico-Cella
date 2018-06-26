5a3abb192739ba662ba947ea1c66a7f219438e6e
federicocella <cella.fed@gmail.com>
2018-06-26 12:36:59 +0200

    Brandon

---
+++ script.js
@@ -0,0 +1,23 @@
+function pageAppear() {
+    $(".black").removeClass("blackon");
+    $(".black").addClass("blackoff");
+}
+window.onload = pageAppear;
+$(document).ready(function(){
+    $("#projects").mouseenter(function() {
+        $("#projline").addClass("underline");
+        $("#projline").removeClass("underlineoff");
+    });
+    $("#projects").mouseleave(function() {
+        $("#projline").removeClass("underline");
+        $("#projline").addClass("underlineoff");
+    });
+    $("#about").mouseenter(function() {
+        $("#aboutline").addClass("underline");
+        $("#aboutline").removeClass("underlineoff");
+    });
+    $("#about").mouseleave(function() {
+        $("#aboutline").removeClass("underline");
+        $("#aboutline").addClass("underlineoff");
+    });
+});
