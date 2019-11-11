2a7ae0c42f11cab2bfad6097a9e592b88a9e690f
federicocella <cella.fed@gmail.com>
2019-11-11 17:31:43 +0100

    Add files via upload

--- Commonplaces/index.js
+++ Commonplaces/index.js
@@ -1,12 +1,21 @@
 var quotes = document.getElementsByClassName("quote");
-for (var i = 0; i < quotes.length; i++) {
-  var shadowProperty = "";
-  for (var g = 1; g < 20; g++) {
-    if (g >= 2) {
-      shadowProperty += ", ";
-    }
-    shadowProperty += "-" + g + "px -" + g + "px var(--shadowColor)";
+var hl = document.getElementById("commonplace");
+var shadowProperty = "";
+var hlShadowProperty = "";
+for (var g = 1; g < 20; g++) {
+  if (g >= 2) {
+    shadowProperty += ", ";
   }
+  shadowProperty += "-" + g + "px -" + g + "px var(--shadowColor)";
+}
+for (var g = 1; g < 10; g++) {
+  if (g >= 2) {
+    hlShadowProperty += ", ";
+  }
+  hlShadowProperty += "-" + g + "px -" + g + "px var(--shadowColor)";
+}
+//hl.style.textShadow = hlShadowProperty;
+for (var i = 0; i < quotes.length; i++) {
   //shadowProperty += ";";
   console.log(shadowProperty);
   quotes[i].style.textShadow = shadowProperty;
